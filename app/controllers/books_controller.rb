20aae6e082dadf865c7e61859bf75bfd6760030c
thomaschoi4002 <thomaschoi4002@gmail.com>
2026-09-20 23:00:33 -0500

    Initial commit, functional book_collection app for Book Collection 1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.order(:id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book created."
+      redirect_to books_path
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book updated."
+      redirect_to books_path
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book deleted."
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(
+      :title
+    )
+  end
+
+end
